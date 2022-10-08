dee8de6407e9a267b7064707864552e14aa84df0
Stefan Hagen <stefan@hagen.link>
2022-10-08 19:24:32 +0200

    Refactored model rep

--- magnetismi/model/cof.py
+++ magnetismi/model/cof.py
@@ -12,24 +12,26 @@ class Coefficients:
 
     years_covered = tuple(y for y in range(2020, 2025 + 1))
     model_from_year = {y: '2020' for y in range(2020, 2025 + 1)}
-    data = []
+    model = {}
 
     def __init__(self, year: int):
         """Verify we cover the year requested and if covered load the coefficients."""
         if year not in self.years_covered:
             raise ValueError(f'requested year ({year}) not within {self.years_covered}')
 
-        self.year = year
-        self.model_year = self.model_from_year[self.year]
-        with open(STORE_PATH / f'wmm-{self.model_year}.txt', 'rt', encoding=ENCODING) as handle:
+        self.model = {
+            'year_requested': year,
+            'model_year': self.model_from_year[year],
+        }
+        with open(STORE_PATH / f'wmm-{self.model["model_year"]}.txt', 'rt', encoding=ENCODING) as handle:
             recs = [line.strip().split() for line in handle if line.strip() and line.strip() != TWIN_END_TOKEN]
 
         epoc, model, modeldate = recs[0]
-        self.epoch = float(epoc)
-        self.model = model
-        self.modeldate = modeldate
+        self.model['epoch'] = float(epoc)
+        self.model['model_code'] = model
+        self.model['model_date_string'] = modeldate
 
-        self.data = [
+        self.model['coeffs'] = [
             {c: int(r[s]) if c in ('n', 'm') else float(r[s]) for s, c in enumerate(CS)}
             for r in recs[1:]
             if len(r) == len(CS)
--- test/test_model.py
+++ test/test_model.py
@@ -19,19 +19,19 @@ def test_coefficients_from_future_after_models_available():
 
 def test_coefficients_from_within_models_available():
     coefficients = cof.Coefficients(2022)
-    assert coefficients.epoch == 2020
-    assert coefficients.model == 'WMM-2020'
-    assert coefficients.modeldate == '12/10/2019'
-    assert len(coefficients.data) == 90
+    assert coefficients.model['epoch'] == 2020
+    assert coefficients.model['model_code'] == 'WMM-2020'
+    assert coefficients.model['model_date_string'] == '12/10/2019'
+    assert len(coefficients.model['coeffs']) == 90
 
 
 def test_coefficients_2020_slot_1_0():
     slot_1_2 = {c: val for c, val in zip(cof.CS, (1, 0, -29404.5, 0.0, 6.7, 0.0))}
     coefficients = cof.Coefficients(2024)
-    assert coefficients.data[0] == slot_1_2
+    assert coefficients.model['coeffs'][0] == slot_1_2
 
 
 def test_coefficients_2020_slot_12_12():
     slot_12_12 = {c: val for c, val in zip(cof.CS, (12, 12, -0.3, 0.5, -0.1, -0.1))}
     coefficients = cof.Coefficients(2024)
-    assert coefficients.data[-1] == slot_12_12
+    assert coefficients.model['coeffs'][-1] == slot_12_12
