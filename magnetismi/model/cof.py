dcffdcc534660bf1e6529a6f2d2ba53aec85385a
Stefan Hagen <stefan@hagen.link>
2022-10-08 19:36:05 +0200

    Refactored the load method to completely load the model

--- magnetismi/model/cof.py
+++ magnetismi/model/cof.py
@@ -14,21 +14,11 @@ class Coefficients:
     model_from_year = {y: '2020' for y in range(2020, 2025 + 1)}
     model = {}
 
-    def load(self, model_year: int) -> list[list[str]]:
+    def load(self, model_year: int) -> None:
         """Load the model."""
         with open(STORE_PATH / f'wmm-{model_year}.txt', 'rt', encoding=ENCODING) as handle:
-            return [line.strip().split() for line in handle if line.strip() and line.strip() != TWIN_END_TOKEN]
+            recs = [line.strip().split() for line in handle if line.strip() and line.strip() != TWIN_END_TOKEN]
 
-    def __init__(self, year: int):
-        """Verify we cover the year requested and if covered load the coefficients."""
-        if year not in self.years_covered:
-            raise ValueError(f'requested year ({year}) not within {self.years_covered}')
-
-        self.model = {
-            'year_requested': year,
-            'model_id': self.model_from_year[year],
-        }
-        recs = self.load(self.model['model_id'])
         epoc, model, modeldate = recs[0]
         self.model['epoch'] = float(epoc)
         self.model['model_code'] = model
@@ -39,3 +29,14 @@ class Coefficients:
             for r in recs[1:]
             if len(r) == len(CS)
         ]
+
+    def __init__(self, year: int):
+        """Verify we cover the year requested and if covered load the coefficients."""
+        if year not in self.years_covered:
+            raise ValueError(f'requested year ({year}) not within {self.years_covered}')
+
+        self.model = {
+            'year_requested': year,
+            'model_id': self.model_from_year[year],
+        }
+        self.load(self.model['model_id'])
