4b38a49ebe516c3e69be155115ea4aa704aa121f
Stefan Hagen <stefan@hagen.link>
2022-10-08 19:32:18 +0200

    Extracted file load from init and found a better name for the mode identifier

--- magnetismi/model/cof.py
+++ magnetismi/model/cof.py
@@ -14,6 +14,11 @@ class Coefficients:
     model_from_year = {y: '2020' for y in range(2020, 2025 + 1)}
     model = {}
 
+    def load(self, model_year: int) -> list[list[str]]:
+        """Load the model."""
+        with open(STORE_PATH / f'wmm-{model_year}.txt', 'rt', encoding=ENCODING) as handle:
+            return [line.strip().split() for line in handle if line.strip() and line.strip() != TWIN_END_TOKEN]
+
     def __init__(self, year: int):
         """Verify we cover the year requested and if covered load the coefficients."""
         if year not in self.years_covered:
@@ -21,11 +26,9 @@ class Coefficients:
 
         self.model = {
             'year_requested': year,
-            'model_year': self.model_from_year[year],
+            'model_id': self.model_from_year[year],
         }
-        with open(STORE_PATH / f'wmm-{self.model["model_year"]}.txt', 'rt', encoding=ENCODING) as handle:
-            recs = [line.strip().split() for line in handle if line.strip() and line.strip() != TWIN_END_TOKEN]
-
+        recs = self.load(self.model['model_id'])
         epoc, model, modeldate = recs[0]
         self.model['epoch'] = float(epoc)
         self.model['model_code'] = model
