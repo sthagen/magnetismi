bcb03b1c63cd5c393e6bb543215e3f15cb3250db
Stefan Hagen <stefan@hagen.link>
2022-10-08 18:34:30 +0200

    Added model reader and tests for the resulting model rep

---
+++ magnetismi/model/cof.py
@@ -0,0 +1,36 @@
+import pathlib
+
+from magnetismi import ENCODING
+
+CS = ('n', 'm', 'gnm', 'hnm', 'dgnm', 'dhnm')
+STORE_PATH = pathlib.Path('magnetismi', 'model')
+TWIN_END_TOKEN = '9' * 48
+
+
+class Coefficients:
+    """The WMM coefficients relevant to the year given."""
+
+    years_covered = tuple(y for y in range(2020, 2025 + 1))
+    model_from_year = {y: '2020' for y in range(2020, 2025 + 1)}
+    data = []
+
+    def __init__(self, year: int):
+        """Verify we cover the year requested and if covered load the coefficients."""
+        if year not in self.years_covered:
+            raise ValueError(f'requested year ({year}) not within {self.years_covered}')
+
+        self.year = year
+        self.model_year = self.model_from_year[self.year]
+        with open(STORE_PATH / f'wmm-{self.model_year}.txt', 'rt', encoding=ENCODING) as handle:
+            recs = [line.strip().split() for line in handle if line.strip() and line.strip() != TWIN_END_TOKEN]
+
+        epoc, model, modeldate = recs[0]
+        self.epoch = float(epoc)
+        self.model = model
+        self.modeldate = modeldate
+
+        self.data = [
+            {c: float(r[s]) if c not in ('n', 'm') else int(r[s]) for s, c in enumerate(CS)}
+            for r in recs[1:]
+            if len(r) == len(CS)
+        ]
---
+++ test/test_model.py
@@ -0,0 +1,37 @@
+import re
+
+import pytest
+
+from magnetismi.model import cof
+
+
+def test_coefficients_from_past_before_models_available():
+    msg = r'requested year (1234) not within (2020, 2021, 2022, 2023, 2024, 2025)'
+    with pytest.raises(ValueError, match=re.escape(msg)):
+        cof.Coefficients(1234)
+
+
+def test_coefficients_from_future_after_models_available():
+    msg = r'requested year (9876) not within (2020, 2021, 2022, 2023, 2024, 2025)'
+    with pytest.raises(ValueError, match=re.escape(msg)):
+        cof.Coefficients(9876)
+
+
+def test_coefficients_from_within_models_available():
+    coefficients = cof.Coefficients(2022)
+    assert coefficients.epoch == 2020
+    assert coefficients.model == 'WMM-2020'
+    assert coefficients.modeldate == '12/10/2019'
+    assert len(coefficients.data) == 90
+
+
+def test_coefficients_2020_slot_1_0():
+    slot_1_2 = {c: val for c, val in zip(cof.CS, (1, 0, -29404.5, 0.0, 6.7, 0.0))}
+    coefficients = cof.Coefficients(2024)
+    assert coefficients.data[0] == slot_1_2
+
+
+def test_coefficients_2020_slot_12_12():
+    slot_12_12 = {c: val for c, val in zip(cof.CS, (12, 12, -0.3, 0.5, -0.1, -0.1))}
+    coefficients = cof.Coefficients(2024)
+    assert coefficients.data[-1] == slot_12_12
